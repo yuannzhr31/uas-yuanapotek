8f8ce8cce0ebabd015de2258b9a59f0045b80447
yuannzhr31 <yuandazahraaulia@gmail.com>
2023-07-07 00:37:55 +0700

    uas Perbaikan

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -14,7 +14,7 @@
 <body>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#"><b>Id Transaksi</b></a>
+            <a class="navbar-brand" href="#"><b>Apotek</b></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
--- resources/views/departements/create.blade.php
+++ resources/views/departements/create.blade.php
@@ -5,8 +5,8 @@
     <div class="row">
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
-                <strong>Departement Name:</strong>
-                <input type="text" name="name" class="form-control" placeholder="Departement Name">
+                <strong>Department Name:</strong>
+                <input type="text" name="name" class="form-control" placeholder="Department Name">
                 @error('name')
                 <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
                 @enderror
@@ -21,16 +21,20 @@
                 @enderror
             </div>
         </div>
-        <div class="form-group">
-            <label for="manager_id">Manager</label>
-            <select name="manager_id" id="manager_id" class="form-select">
-                <option value="">Pilih</option>
-                @foreach ($managers as $manager)
-                <option value="{{ $manager->id }}">{{ $manager->name }}</option>
-                @endforeach
-            </select>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <label for="manager_id">Manager</label>
+                <select name="manager_id" id="manager_id" class="form-select">
+                    <option value="">Pilih</option>
+                    @foreach ($managers as $manager)
+                    <option value="{{ $manager->id }}">{{ $manager->name }}</option>
+                    @endforeach
+                </select>
+            </div>
         </div>
-        <button type="submit" class="btn btn-primary mt-3 ml-3">Submit</button>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <button type="submit" class="btn btn-primary mt-3 ml-3">Submit</button>
+        </div>
     </div>
 </form>
 @endsection
